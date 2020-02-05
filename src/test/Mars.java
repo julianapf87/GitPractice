254dad28aa61ca87acd80917ebbfad40988dd9ac
julianapf87 <julianapf87@gmail.com>
2020-02-04 21:20:40 -0500

    This is my last commit

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -2,6 +2,7 @@ package test;
 
 public class Mars {
 
+<<<<<<< Updated upstream
   public static void main (String[] args){
   System.out.println ("Hello World");
   
@@ -11,4 +12,16 @@ public class Mars {
     
    System.out.println ("Hello");
   }
+=======
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Herrow");
+	
+	
+		
+		
+	
+	}
+
+>>>>>>> Stashed changes
 }
