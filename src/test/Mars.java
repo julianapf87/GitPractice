1ac6103b7963cc3ae13eee3f8c73fa2414a9dafa
julianapf87 <julianapf87@gmail.com>
2020-02-04 21:18:59 -0500

    my local commit

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -3,12 +3,12 @@ package test;
 public class Mars {
 
   public static void main (String[] args){
-  System.out.println ("Hello World")
+  System.out.println ("Hello World");
   
     
     
     
     
-   System.out.println ("Hello")
+   System.out.println ("Hello");
   }
 }
